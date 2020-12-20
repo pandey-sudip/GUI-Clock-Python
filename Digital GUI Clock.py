14de6a7aee5c310132ff48acafb04668717912a5
Technical-Vandar-885 <73782935+Technical-Vandar-885@users.noreply.github.com>
2020-12-20 09:02:34 +0545

    Add files via upload

---
+++ Digital GUI Clock.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import time
+
+def times():
+    currentTime=time.strftime("%H: %M: %S %p")
+    label.config(text=currentTime)
+    label.after(200, times)
+
+root=Tk()
+
+root.title("Python GUI Digital Clock")
+root.wm_iconbitmap("clock.ico")
+root.geometry("700x200")
+root.minsize(700, 200)
+root.maxsize(700, 200)
+
+label=Label(root, font="ds-digital 100 bold", bg="black", foreground="red")
+label.pack(pady=40)
+
+times()
+root.mainloop()
